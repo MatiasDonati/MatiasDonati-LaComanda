b7598f1101bf044576f5a56eb154ff3cebf06da4
Matias <matod87@hotmail.com>
2024-11-06 16:33:34 -0300

    mw consulta pedidos

--- app/middlewares/PedidosMiddleware.php
+++ app/middlewares/PedidosMiddleware.php
@@ -5,6 +5,8 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
 
 class CrearPedidoMiddleware
+
+// 
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
